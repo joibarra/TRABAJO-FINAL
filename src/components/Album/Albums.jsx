170be37d69e09bee357349d82d7a6f33d5570664
maxiq1989 <maxiq1989@gmail.com>
2024-08-11 20:46:46 -0300

    Se arregla colores de fondo

--- src/components/Album/Albums.jsx
+++ src/components/Album/Albums.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Table,
   TableBody,
@@ -36,15 +36,15 @@ export default function Albums() {
   };
   // Definir las rutas de menú manualmente si no están bajo un padre común
   const menuItems = [
-    {path: "/albums", label: "Album"},
-    {path: "/songs", label: "Canciones"},
-    {path: "/ArtistList", label: "Artistas"},
-  //  {path: "/BuscadorDeCanciones", label: "BuscadorDeCanciones"},
+    { path: "/albums", label: "Album" },
+    { path: "/songs", label: "Canciones" },
+    { path: "/ArtistList", label: "Artistas" },
+    //  {path: "/BuscadorDeCanciones", label: "BuscadorDeCanciones"},
   ];
- //para reveer
+  //para reveer
   const addAlbum = () => {
-    console.log("entro a album")
-   setShowPopupCreate(true);
+    console.log("entro a album");
+    setShowPopupCreate(true);
   };
   function handleSearch(event) {
     event.preventDefault();
@@ -94,7 +94,7 @@ export default function Albums() {
                 }
               );
               const artistData = await artistResponse.json();
-              return {...album, artistName: artistData.name};
+              return { ...album, artistName: artistData.name };
             })
           );
           setAlbums(albumsWithArtists);
@@ -144,9 +144,9 @@ export default function Albums() {
     setShowPopup(false);
   }
 
-  function handlePopupCreate (){
-    console.log("entro a handlePopupCreate ")
-    setShowPopupCreate (true);
+  function handlePopupCreate() {
+    console.log("entro a handlePopupCreate ");
+    setShowPopupCreate(true);
   }
 
   function handleClosePopupAlbum() {
@@ -154,10 +154,10 @@ export default function Albums() {
   }
 
   return (
-    <div style={{display: "flex", height: "100vh"}}>
-      <aside style={{width: "200px", background: "#A5FFC9", padding: "10px"}}>
+    <div style={{ display: "flex", minHeight: "100vh" }}>
+      <aside style={{ width: "200px", background: "#A5FFC9", padding: "10px" }}>
         <nav>
-          <ul style={{listStyle: "none", padding: 0}}>
+          <ul style={{ listStyle: "none", padding: 0 }}>
             {menuItems.map((item, index) => (
               <li key={index}>
                 <Link to={item.path}>{item.label}</Link>
@@ -166,7 +166,7 @@ export default function Albums() {
           </ul>
         </nav>
       </aside>
-      <main style={{flex: 1, background: "#222222", padding: "10px"}}>
+      <main style={{ flex: 1, background: "#222222", padding: "10px" }}>
         <header
           style={{
             display: "flex",
@@ -174,98 +174,124 @@ export default function Albums() {
             alignItems: "center",
           }}
         >
-          <h1 style={{ color: "#61F2B1", fontSize: "24px", fontWeight: "bold"}}>Albums</h1>
-          <button style={{  marginTop: "20px", padding: "10px 20px", backgroundColor: "#333", color: "#61F2B1", border: "none",  borderRadius: "5px", cursor: "#61F2B1", alignSelf: "flex-end", border: "2px solid black", }} onClick={handleLogout}>Cerrar Sesión</button>
+          <h1
+            style={{
+              color: "#61F2B1",
+              fontSize: "24px",
+              fontWeight: "bold",
+            }}
+          >
+            Albums
+          </h1>
+          <button
+            style={{
+              marginTop: "20px",
+              padding: "10px 20px",
+              backgroundColor: "#333",
+              color: "#61F2B1",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "#61F2B1",
+              alignSelf: "flex-end",
+              border: "2px solid black",
+            }}
+            onClick={handleLogout}
+          >
+            Cerrar Sesión
+          </button>
         </header>
         <form action="">
-        <fieldset>
-    <div style={{ flex: 1, marginRight: "10px", color: "#61F2B1" }}>
-    <label>Título: 
-      <input
-        type="text"
-        name="title"
-        style={{
-          marginLeft: "10px",
-          width: "calc(100% - 20px)",
-          padding: "5px",
-          border: "1px solid black",
-        }}
-      />
-      </label>
-    <label>Artista:
-      <input
-        type="text"
-        name="artist"
-        style={{
-          marginLeft: "10px",
-          width: "calc(100% - 20px)",
-          padding: "5px",
-          border: "1px solid black",
-        }}
-      />
-    </label>
-    <label > Año:
-      <input
-        type="text"
-        name="year"
-        style={{
-          marginLeft: "10px",
-          width: "calc(100% - 20px)",
-          padding: "5px",
-          border: "1px solid black",
-        }}
-      />
-    </label>
-    <label >
-      Creado por:
-      <input
-        type="text"
-        name="createdBy"
-        style={{
-          marginLeft: "10px",
-          width: "calc(100% - 20px)",
-          padding: "5px",
-          border: "1px solid black",
-        }}
-      />
-    </label>
-    </div>
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-    <button
-      onClick={handleSearch}
-      style={{
-        marginTop: "20px",
-        padding: "10px 20px",
-        backgroundColor: "#333",
-        color: "#61F2B1",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        alignSelf: "flex-end",
-        border: "2px solid black",
-      }}
-    >
-      Buscar
-    </button>
-    <button onClick={addAlbum}
-        style={{
-          marginTop: "20px",
-          padding: "10px 20px",
-          backgroundColor: "#333",
-          color: "#61F2B1",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          alignSelf: "flex-end",
-          border: "2px solid black",
-        }}
-      >
-        Agregar album
-      </button>
-      </div>
-     
-  </fieldset>
-</form>
+          <fieldset>
+            <div style={{ flex: 1, marginRight: "10px", color: "#61F2B1" }}>
+              <label>
+                Título:
+                <input
+                  type="text"
+                  name="title"
+                  style={{
+                    marginLeft: "10px",
+                    width: "calc(100% - 20px)",
+                    padding: "5px",
+                    border: "1px solid black",
+                  }}
+                />
+              </label>
+              <label>
+                Artista:
+                <input
+                  type="text"
+                  name="artist"
+                  style={{
+                    marginLeft: "10px",
+                    width: "calc(100% - 20px)",
+                    padding: "5px",
+                    border: "1px solid black",
+                  }}
+                />
+              </label>
+              <label>
+                Año:
+                <input
+                  type="text"
+                  name="year"
+                  style={{
+                    marginLeft: "10px",
+                    width: "calc(100% - 20px)",
+                    padding: "5px",
+                    border: "1px solid black",
+                  }}
+                />
+              </label>
+              <label>
+                Creado por:
+                <input
+                  type="text"
+                  name="createdBy"
+                  style={{
+                    marginLeft: "10px",
+                    width: "calc(100% - 20px)",
+                    padding: "5px",
+                    border: "1px solid black",
+                  }}
+                />
+              </label>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <button
+                onClick={handleSearch}
+                style={{
+                  marginTop: "20px",
+                  padding: "10px 20px",
+                  backgroundColor: "#333",
+                  color: "#61F2B1",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  alignSelf: "flex-end",
+                  border: "2px solid black",
+                }}
+              >
+                Buscar
+              </button>
+              <button
+                onClick={addAlbum}
+                style={{
+                  marginTop: "20px",
+                  padding: "10px 20px",
+                  backgroundColor: "#333",
+                  color: "#61F2B1",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  alignSelf: "flex-end",
+                  border: "2px solid black",
+                }}
+              >
+                Agregar album
+              </button>
+            </div>
+          </fieldset>
+        </form>
 
         <div>
           <TableContainer component={Paper}>
@@ -334,9 +360,8 @@ export default function Albums() {
           {showPopup && (
             <PopupMsj message={message} onClose={handleClosePopup} />
           )}
-          
         </div>
       </main>
     </div>
-  ); 
+  );
 }
